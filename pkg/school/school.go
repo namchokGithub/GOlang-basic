169112427167802104b6577a2259697c14e71980
Namchok <Pech4751@gmail.com>
2021-05-05 10:23:57 +0700

    Update

--- pkg/school/school.go
+++ pkg/school/school.go
@@ -1,7 +1,7 @@
 package school
 
-SchoolName := "เตรียมอุดมศึกษา";
+var SchoolName string = "เตรียมอุดมศึกษา"
 
 func GetSchoolAddressSchool() string {
-    return "กรุงเทพ";
+	return "กรุงเทพ" + SchoolName
 }
--- src/Test/Main.go
+++ src/Test/Main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	// "../pkg/school"
 )
 
 type humen struct {
